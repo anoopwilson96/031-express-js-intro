0c0803446ae994f22d4ef60708b8403a6663806b
Anoop Wilson <anoopwilson33@yahoo.com>
2024-05-09 00:14:41 +0530

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,31 +4,31 @@ const router = express.Router()
 
 //Get all categories
 
-router.get('/categories',(req,res)=>{
+router.get('/',(req,res)=>{
   res.send('Not written')
 })
 
 // Get category by Id
 
-router.get('/categories/:categoryId',(req,res)=>{
+router.get('/:categoryId',(req,res)=>{
   res.send('Not written')
 })
 
 // Add new category
 
-router.post('/categories/:categoryId',(req,res)=>{
+router.post('/:categoryId',(req,res)=>{
   res.send('Not written')
 })
 
 // Update category
 
-router.patch('/categories/:categoryId',(req,res)=>{
+router.patch('/:categoryId',(req,res)=>{
   res.send('Not written')
 })
 
 // Delete a category
 
-router.delete('/categories/:categoryId',(req,res)=>{
+router.delete('/:categoryId',(req,res)=>{
   res.send('Not written')
 })
 
