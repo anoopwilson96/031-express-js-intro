577ba0dec65b74b80a9a44ed5bf08470a3fa8453
Anoop Wilson <anoopwilson33@yahoo.com>
2024-05-11 07:51:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose');
 const productRoutes = require('./routes/productRoutes')
 const categoryRoutes = require('./routes/categoryRoutes')
 const port = 3000
@@ -15,3 +16,15 @@ app.use('/categories',categoryRoutes)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
+// getting-started.js
+
+
+main() .then(()=>{console.log('connected')})  .catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://031expressjsintro:cKIntvHyKAUab75Y@cluster0.mjw1uqj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+}
